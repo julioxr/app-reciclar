796e306398c09e64404325c7bd90065600ba00ad
julio <julioroja987@gmail.com>
2023-08-09 14:41:28 -0300

    sad

--- src/app/page.js
+++ src/app/page.js
@@ -1,44 +1,15 @@
-import React, { useEffect, useRef } from "react";
-
-function CameraCapture() {
-    const videoRef = useRef();
-    const canvasRef = useRef();
-
-    useEffect(() => {
-        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-            navigator.mediaDevices
-                .getUserMedia({ video: true })
-                .then((stream) => {
-                    videoRef.current.srcObject = stream;
-                })
-                .catch((error) =>
-                    console.error("Error accessing camera:", error)
-                );
-        }
-    }, []);
-
-    const capturePhoto = () => {
-        const canvas = canvasRef.current;
-        const context = canvas.getContext("2d");
-        context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
-
-        // Aquí puedes enviar la imagen capturada a tu servidor o realizar cualquier otra acción
-    };
-
-    return (
-        <div>
-            <h1>Camera Capture</h1>
-            <video ref={videoRef} autoPlay></video>
-            <button onClick={capturePhoto}>Capture Photo</button>
-            <canvas ref={canvasRef} style={{ display: "none" }}></canvas>
-        </div>
-    );
-}
+"use client";
+import Webcam from "react-webcam";
+import { useState, useRef } from "react";
 
 export default function Home() {
+    const [showWebcam, setShowWebcam] = useState(false);
     return (
         <div>
-            <CameraCapture />
+            <h1>App</h1>
+            {showWebcam ? <Webcam /> : ""}
+
+            <button onClick={() => setShowWebcam(true)}>Abrir Camara</button>
         </div>
     );
 }
